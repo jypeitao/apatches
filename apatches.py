696d15f9b2070732767be80c566f1264c11ed168
tao.pei <gpeitao@gmail.com>
2018-05-18 08:47:09 +0800

    format code

--- apatches.py
+++ apatches.py
@@ -13,7 +13,6 @@ def get_all_files(top):
     all_files = []
     for (dirpath, dirnames, filenames) in os.walk(top):
         for file in filenames:
-            # all_files.append(os.path.join(dirpath[len(top) + 1:], file))
             all_files.append(os.path.join(dirpath, file))
 
     all_files.sort()
@@ -98,15 +97,15 @@ def get_git_directory(patch):
 
 
 def color_string(content, color):
-    COLOR = { 
-            'black': 30,   # 黑色
-            'red': 31,   # 红色
-            'green': 32,   # 绿色
-            'yellow': 33,   # 黄色
-            'blue': 34,   # 蓝色
-            'purple': 35,   # 紫红色
-            'cyan': 36,   # 青蓝色
-            'white': 37,   # 白色
+    COLOR = {
+        'black': 30,  # 黑色
+        'red': 31,  # 红色
+        'green': 32,  # 绿色
+        'yellow': 33,  # 黄色
+        'blue': 34,  # 蓝色
+        'purple': 35,  # 紫红色
+        'cyan': 36,  # 青蓝色
+        'white': 37,  # 白色
     }
     cstring = "\033[0;" + str(COLOR[color]) + "m" + content + "\033[0m"
     return cstring
@@ -116,6 +115,7 @@ def apply_patch(patch):
     cmd = "git am --directory=" + get_git_directory(patch) + " -k " + patch
     print(cmd)
 
+
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         print('''Usage: apatches your_patches_folder''')
@@ -123,9 +123,6 @@ if __name__ == '__main__':
 
     # make sure the path has a sep
     patches_folder = os.path.join(sys.argv[1], "")
-    # old_cwd = os.getcwd()
-    # os.chdir("C:\\Users\\admin\\Desktop")
-
 
     # use map to record the state of each patch.
     # key is patch name
@@ -149,7 +146,6 @@ if __name__ == '__main__':
             patches_info[prefixless].append('not applied')
             patches_info[prefixless].append('already exist')
 
-
     need_apply_patches.sort()
     if not need_apply_patches:
         print(color_string("No patch needs to be applied.", 'green'))
@@ -162,22 +158,12 @@ if __name__ == '__main__':
         print(color_string("Applying: [" + str(i) + "/" + str(total_patches) + "]", 'green') + patch)
         cmd = "git am --directory=" + get_git_directory(patch[len(patches_folder):]) + " -k " + patch
         git_result = shell_exc(cmd)
-        #print(git_result)
-        #print(cmd)
+        # print(git_result)
+        # print(cmd)
         if has_error(git_result):
             shell_exc("git am --abort")
-            error_info = "Error: Already applied " + str(i - 1) +", " + str(total_patches -i +1) + " left to apply."
-            print(color_string("Error: Already applied " + str(i - 1) +", " + str(total_patches -i +1) + " left to apply.", 'red'))
+            print(color_string(
+                "Error: Already applied " + str(i - 1) + ", " + str(total_patches - i + 1) + " left to apply.", 'red'))
             print(color_string("Please manually execute:" + cmd + " and fix it.", 'red'))
             print(color_string("Then re-execute the command:" + sys.argv[0] + " " + sys.argv[1], 'red'))
             exit(-1)
-
-    # just test
-    #patch = need_apply_patches[0]
-    #print(patch)
-    #cmd = "git am --directory=" + get_git_directory(patch[len(patches_folder):]) + " -k " + patch
-    #print(cmd)
-    #git_result = shell_exc(cmd)
-    #print(git_result)
-    #if has_error(git_result):
-    #    exit(-1)
